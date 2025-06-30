a5ec0a624146264dfcfc267c2f2ce11cfe6aab1b
Baho Philomé Evrard <157682613+ApprentieDeveloppeurEvrard@users.noreply.github.com>
2025-06-30 15:21:08 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function HomePage() {
       <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-200 font-medium relative z-10 bg-white backdrop-blur-md">
         {/* Logo */}
         <a href="/" className="text-2xl font-bold text-[#18181b] hover:text-black transition-colors">
-        Planify
+        NextTask
         </a>
 
         {/* Hamburger (Mobile) */}
